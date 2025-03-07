7820cfe8ba669781eaf7ad5c9b541365de264ef9
awang-25 <aplacaswine@gmail.com>
2025-03-07 03:06:50 +0000

    submit

--- workspace/Board.java
+++ workspace/Board.java
@@ -1,4 +1,4 @@
-
+// Name: Adam Wang
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -32,6 +32,8 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 	private static final String RESOURCES_WQUEEN_PNG = "wqueen.png";
 	private static final String RESOURCES_WPAWN_PNG = "wpawn.png";
 	private static final String RESOURCES_BPAWN_PNG = "bpawn.png";
+
+    private static final String RESOURCES_WJONKLER_PNG = "thejonkler.png";
 	
 	// Logical and graphical representations of board
 	private final Square[][] board;
@@ -48,7 +50,7 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     private int currX;
     private int currY;
     
-
+    private Square sq;
     
     public Board(GameWindow g) {
         this.g = g;
@@ -60,7 +62,27 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 
         //TO BE IMPLEMENTED FIRST
      
-      //for (.....)  
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(i % 2 == 0){
+                    if(j % 2 == 0){
+                        board[i][j] = new Square(this, true, i, j);
+                        this.add(board[i][j]);
+                    } else {
+                        board[i][j] = new Square(this, false, i, j);
+                        this.add(board[i][j]);
+                    }
+                } else {
+                    if(j % 2 == 0){
+                        board[i][j] = new Square(this, false, i, j);
+                        this.add(board[i][j]);
+                    } else {
+                        board[i][j] = new Square(this, true, i, j);
+                        this.add(board[i][j]);
+                    }
+                }
+            }
+        }  
 //        	populate the board with squares here. Note that the board is composed of 64 squares alternating from 
 //        	white to black.
 
@@ -79,10 +101,32 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
 	//set up the board such that the black pieces are on one side and the white pieces are on the other.
 	//since we only have one kind of piece for now you need only set the same number of pieces on either side.
 	//it's up to you how you wish to arrange your pieces.
+    // preconditions: row and columb values must be greater than or equal to 0 and less than 8
+    // postcondition: all pieces are shown on the board
     private void initializePieces() {
-    	
-    	board[0][0].put(new Piece(true, RESOURCES_WKING_PNG));
-
+    	board[0][0].put(new Piece(false, RESOURCES_BROOK_PNG));
+        board[0][1].put(new Piece(false, RESOURCES_BKNIGHT_PNG));
+        board[0][2].put(new Piece(false, RESOURCES_BBISHOP_PNG));
+        board[0][3].put(new Piece(false, RESOURCES_BQUEEN_PNG));
+        board[0][4].put(new Piece(false, RESOURCES_BKING_PNG));
+        board[0][5].put(new Piece(false, RESOURCES_BBISHOP_PNG));
+        board[0][6].put(new Piece(false, RESOURCES_BKNIGHT_PNG));
+        board[0][7].put(new Piece(false, RESOURCES_BROOK_PNG));
+        for(int i = 0; i < 8; i++){
+            board[1][i].put(new Piece(false, RESOURCES_BPAWN_PNG));
+        }
+        board[7][0].put(new Piece(true, RESOURCES_WROOK_PNG));
+        board[7][1].put(new Piece(true, RESOURCES_WKNIGHT_PNG));
+        board[7][2].put(new Piece(true, RESOURCES_WBISHOP_PNG));
+        board[7][3].put(new Piece(true, RESOURCES_WQUEEN_PNG));
+        board[7][4].put(new Piece(true, RESOURCES_WKING_PNG));
+        board[7][5].put(new Piece(true, RESOURCES_WBISHOP_PNG));
+        board[7][6].put(new Piece(true, RESOURCES_WKNIGHT_PNG));
+        board[7][7].put(new Piece(true, RESOURCES_WROOK_PNG));
+        for(int j = 0; j < 8; j++){
+            board[6][j].put(new Piece(true, RESOURCES_WPAWN_PNG));
+        }
+        board[3][3].put(new Piece(true, RESOURCES_WJONKLER_PNG));
     }
 
     public Square[][] getSquareArray() {
@@ -122,47 +166,77 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         }
         
     }
-
+    
     @Override
     public void mousePressed(MouseEvent e) {
-        currX = e.getX();
-        currY = e.getY();
+       
+            currX = e.getX();
+            currY = e.getY();
 
-        Square sq = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
-
-        if (sq.isOccupied()) {
-            currPiece = sq.getOccupyingPiece();
-            fromMoveSquare = sq;
-            if (!currPiece.getColor() && whiteTurn)
-                return;
-            if (currPiece.getColor() && !whiteTurn)
-                return;
-            sq.setDisplay(false);
-        }
-        repaint();
+            sq = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
+            
+            if (sq.isOccupied()) {
+                currPiece = sq.getOccupyingPiece();
+                fromMoveSquare = sq;
+                if (currPiece.getColor() !=whiteTurn){
+                    currPiece = null;
+                    return;
+                }
+               
+                sq.setDisplay(false);
+            }
+            repaint();
+        
+        
     }
 
     //TO BE IMPLEMENTED!
     //should move the piece to the desired location only if this is a legal move.
     //use the pieces "legal move" function to determine if this move is legal, then complete it by
-    //moving the new piece to it's new board location. 
+    //moving the new piece to its new board location. 
     @Override
+    // Called when user lets go of piece
+    // preconditoins: piece has been picked up, endquare is on a square with row and columb greater than or equal to 0 and less than 8
+    // postcondition: if piece has been moved to a legal square, then it is dropped there; if not it goes back to original position
     public void mouseReleased(MouseEvent e) {
-        Square endSquare = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
+        if(currPiece != null){
+            
+            Square endSquare = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
         
-        //using currPiece
-        
-       
-        fromMoveSquare.setDisplay(true);
-        currPiece = null;
-        repaint();
+            //using currPiece
+            for(Square [] row: board) {
+        	    for(Square s: row) {
+        		    s.setBorder(null);
+                    if(currPiece.getLegalMoves(this,sq).contains(endSquare)){
+                        endSquare.put(currPiece);
+                        currPiece.changeType();
+                        fromMoveSquare.removePiece();
+                        whiteTurn = !whiteTurn;
+                    } else {
+                        sq = fromMoveSquare;
+                    }
+        	    }
+            }
+            fromMoveSquare.setDisplay(true);
+            currPiece = null;
+            repaint();
+        }  
     }
 
     @Override
+    // Called when user is holding the piece and moving the mouse
     public void mouseDragged(MouseEvent e) {
+        if(currPiece != null){
         currX = e.getX() - 24;
         currY = e.getY() - 24;
-
+        if(currPiece!= null) {
+        	for(Square s: currPiece.getLegalMoves(this, fromMoveSquare)) {
+        		s.setBorder(BorderFactory.createLineBorder(Color.MAGENTA));
+        	}
+        	
+        }
+        
+    }
         repaint();
     }
 
--- workspace/Piece.java
+++ workspace/Piece.java
@@ -1,4 +1,4 @@
-
+// Name:Adam Wang
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -14,10 +14,11 @@ import javax.imageio.ImageIO;
 public class Piece {
     private final boolean color;
     private BufferedImage img;
-    
+    private int moveType;
+
     public Piece(boolean isWhite, String img_file) {
         this.color = isWhite;
-        
+        moveType = (int)(Math.random()*3);
         try {
             if (this.img == null) {
               this.img = ImageIO.read(getClass().getResource(img_file));
@@ -49,10 +50,155 @@ public class Piece {
     // TO BE IMPLEMENTED!
     //return a list of every square that is "controlled" by this piece. A square is controlled
     //if the piece capture into it legally.
+    // precondition: row and column of square are greater than or equal to 0 and less than 8
+    // postcondition: returns an arraylist of all squares controled by piece
     public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
-     return null;
+      ArrayList<Square> moves = new ArrayList<Square>();
+      if(moveType == 0){ // ROOK-----------------------------------------------------------------------------------
+        int startRow = start.getRow();
+        int startCol = start.getCol();
+        for(int r = startRow+1; r < 8; r++){ // Squares below piece
+          Square sq = board[r][startCol];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+        }
+        for(int r = startRow-1; r >= 0; r--){ // Squares above piece
+          Square sq = board[r][startCol];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+        }
+        for(int c = startCol+1; c < 8; c++){ // Squares right of piece
+          Square sq = board[startRow][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+        }
+        for(int c = startCol-1; c >= 0; c--){ // Squares left of piece
+          Square sq = board[startRow][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+        }
+      
+      } else if(moveType == 1){ // KNIGHT-----------------------------------------------------------------------------------
+        int row = start.getRow();
+        int col = start.getCol();
+      if(row + 2 < 8 && col - 1 >= 0){ // Down two, left one
+        Square sq = board[row+2][col-1]; // Up two, left one
+        moves.add(sq);
+      }
+      if(row + 2 < 8 && col + 1 < 8){
+        Square sq = board[row+2][col+1]; // Down two, right one
+        moves.add(sq);
+      }
+      if(row - 2 >= 0 && col - 1 >= 0){
+        Square sq = board[row-2][col-1]; // Down two, left one
+        moves.add(sq);
+      }
+      if(row - 2 >= 0 && col + 1 < 8){
+        Square sq = board[row-2][col+1]; // Down two, right one
+        moves.add(sq);
+      }
+      if(row - 1 >= 0 && col + 2 < 8){ 
+        Square sq = board[row-1][col+2]; // Right two, up one
+        moves.add(sq);
+      }
+      if(row + 1 < 8 && col + 2 < 8){ // Right two, down one
+        Square sq = board[row+1][col+2]; // Down two, right one
+        moves.add(sq);
+      }
+      if(row - 1 >= 0 && col - 2 >= 0){ 
+        Square sq = board[row-1][col-2]; // Left two, up one
+        moves.add(sq);
+      }
+      if(row + 1 < 8 && col - 2 >= 0){ 
+        Square sq = board[row+1][col-2]; // Left two, down one
+        moves.add(sq);
+      }
+    } else if(moveType == 2){ // BISHOP-----------------------------------------------------------------------------------
+      int r = start.getRow()+1;
+      int c = start.getCol()+1;
+      for(int i = start.getRow(); i < 8; i++){ // Down right diagonal
+        if(r < 8 && c < 8){
+          Square sq = board[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+            r++;
+            c++;
+        }
+      }
+      r = start.getRow()-1;
+      c = start.getCol()-1;
+      for(int i = start.getRow(); i >= 0; i--){ // Up right diagonal
+        if(r >= 0 && c >= 0){
+          Square sq = board[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+            r--;
+            c--;
+        }
+      }
+      r = start.getRow()+1;
+      c = start.getCol()-1;
+      for(int i = start.getRow(); i < 8; i++){ // Down left diagonal
+        if(r < 8 && c >= 0){
+          Square sq = board[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+            r++;
+            c--;
+        }
+      }
+      r = start.getRow()-1;
+      c = start.getCol()+1;
+      for(int i = start.getRow(); i >= 0; i--){ // Up right diagonal
+        if(r >= 0 && c < 8){
+          Square sq = board[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied()){
+            moves.add(sq);
+            break;
+          }
+            r--;
+            c++;
+          }
+        }
+      }
+      return moves;
     }
     
+
+    public void changeType(){
+      moveType = (int)(Math.random()*3);
+    }
+
 
     //TO BE IMPLEMENTED!
     //implement the move function here
@@ -60,7 +206,197 @@ public class Piece {
     //returns an arraylist of squares which are legal to move to
     //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
     //going to score any points.
+    // Preconditions: row and column are greater than or equal to 0 and less than 8
+    // Postcondition: returns an arraylist of possible moves
+    // Rules: Each turn, the piece can move like a randomly-chosen piece on the board as long as it does not move off the board
+    // 0 = rook, 1 = knight, 2 = bishop
     public ArrayList<Square> getLegalMoves(Board b, Square start){
-    	return null;
-    }
-}
+      ArrayList<Square> moves = new ArrayList<Square>();
+      if(moveType == 0){ // ROOK-----------------------------------------------------------------------------------
+        int startRow = start.getRow();
+        int startCol = start.getCol();
+        for(int r = startRow+1; r < 8; r++){ // Squares below piece
+          Square sq = b.getSquareArray()[r][startCol];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (start.getColor() != sq.getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied () && (start.getColor() == sq.getColor())){
+            break;
+          }
+        }
+        for(int r = startRow-1; r >= 0; r--){ // Squares above piece
+          Square sq = b.getSquareArray()[r][startCol];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (start.getColor() != sq.getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied () && (start.getColor() == sq.getColor())){
+            break;
+          }
+        }
+        for(int c = startCol+1; c < 8; c++){ // Squares right of piece
+          Square sq = b.getSquareArray()[startRow][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied () && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+        }
+        for(int c = startCol-1; c >= 0; c--){ // Squares left of piece
+          Square sq = b.getSquareArray()[startRow][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color!= sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied () && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+        }
+      
+      } else if(moveType == 1){ // KNIGHT-----------------------------------------------------------------------------------
+        int row = start.getRow();
+        int col = start.getCol();
+      if(row + 2 < 8 && col - 1 >= 0){ // Down two, left one
+        Square sq = b.getSquareArray()[row+2][col-1]; // Up two, left one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row + 2 < 8 && col + 1 < 8){
+        Square sq = b.getSquareArray()[row+2][col+1]; // Down two, right one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row - 2 >= 0 && col - 1 >= 0){
+        Square sq = b.getSquareArray()[row-2][col-1]; // Down two, left one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row - 2 >= 0 && col + 1 < 8){
+        Square sq = b.getSquareArray()[row-2][col+1]; // Down two, right one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row - 1 >= 0 && col + 2 < 8){ 
+        Square sq = b.getSquareArray()[row-1][col+2]; // Right two, up one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row + 1 < 8 && col + 2 < 8){ // Right two, down one
+        Square sq = b.getSquareArray()[row+1][col+2]; // Down two, right one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row - 1 >= 0 && col - 2 >= 0){ 
+        Square sq = b.getSquareArray()[row-1][col-2]; // Left two, up one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+      if(row + 1 < 8 && col - 2 >= 0){ 
+        Square sq = b.getSquareArray()[row+1][col-2]; // Left two, down one
+        if(!sq.isOccupied()){
+          moves.add(sq);
+        } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+          moves.add(sq);
+        }
+      }
+    } else if(moveType == 2){ // BISHOP-----------------------------------------------------------------------------------
+      int r = start.getRow()+1;
+      int c = start.getCol()+1;
+      for(int i = start.getRow(); i < 8; i++){ // Down right diagonal
+        if(r < 8 && c < 8){
+          Square sq = b.getSquareArray()[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied() && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+            r++;
+            c++;
+        }
+      }
+      r = start.getRow()-1;
+      c = start.getCol()-1;
+      for(int i = start.getRow(); i >= 0; i--){ // Up right diagonal
+        if(r >= 0 && c >= 0){
+          Square sq = b.getSquareArray()[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied() && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+            r--;
+            c--;
+        }
+      }
+      r = start.getRow()+1;
+      c = start.getCol()-1;
+      for(int i = start.getRow(); i < 8; i++){ // Down left diagonal
+        if(r < 8 && c >= 0){
+          Square sq = b.getSquareArray()[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied() && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+            r++;
+            c--;
+        }
+      }
+      r = start.getRow()-1;
+      c = start.getCol()+1;
+      for(int i = start.getRow(); i >= 0; i--){ // Up right diagonal
+        if(r >= 0 && c < 8){
+          Square sq = b.getSquareArray()[r][c];
+          if(!sq.isOccupied()){
+            moves.add(sq);
+          } else if(sq.isOccupied() && (color != sq.getOccupyingPiece().getColor())){
+            moves.add(sq);
+            break;
+          } else if(sq.isOccupied() && (color == sq.getOccupyingPiece().getColor())){
+            break;
+          }
+            r--;
+            c++;
+          }
+        }
+      }
+    return moves;
+  }
+}   
